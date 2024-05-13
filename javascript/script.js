3e6e419422610366fb77bf88772e8547b855460c
AldyyNgere <aldyngere20@gmail.com>
2024-05-13 17:41:13 +0800

    menyelesaikan akses ke data books.json

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,35 @@ function openMenu(el) {
       navMenu.classList.remove("top-[76px]"),
       navMenu.classList.remove("opacity-100"));
 }
+
+// BOOKS FETCH
+// AJAX REQUEST FOR FETCHING BOOKS DATA
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const booksData = JSON.parse(xhr.responseText);
+    displayBooks(booksData);
+  }
+};
+xhr.open("GET", "json/books.json", true);
+xhr.send();
+
+function displayBooks(books) {
+  const dataBooks = books.cerita_horor;
+  dataBooks.forEach((book) => {
+    const booksContainer = document.querySelector(".book-container");
+    const {id, judul, deskripsi, img} = book;
+    let card = document.createElement("div");
+    card.className = "shadow-lg";
+    card.innerHTML = `<div
+    class="bg-[url(${img})] bg-center bg-cover relative rounded-md cursor-pointer overflow-hidden z-[-2] h-[250px] w-[180px]"
+  >
+    <button
+      class="bg-red-600 absolute top-0 right-0 py-2 px-4 text-xl rounded-bl-lg text-slate-50 hover:bg-red-700 duration-300"
+    >
+      <ion-icon name="book-outline"></ion-icon>
+    </button>
+  </div>`;
+    booksContainer.appendChild(card);
+  });
+}
