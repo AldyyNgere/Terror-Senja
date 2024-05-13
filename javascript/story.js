ae0d9bf57a1da7c1706d2fd05f73e1059b688da2
AldyyNgere <aldyngere20@gmail.com>
2024-05-14 01:37:16 +0800

    menambah data dan audio backsound

--- javascript/story.js
+++ javascript/story.js
@@ -16,6 +16,9 @@ xhr.send();
 
 // DISPLAY BOOK
 function displayBook(book) {
+  const audio = new Audio();
+  audio.src = "audio/horror-backsound.mp3";
+  audio.play();
   const {id, judul, penulis, deskripsi, img} = book;
   const container = document.querySelector(".section-container");
   container.innerHTML = "";
@@ -24,8 +27,8 @@ function displayBook(book) {
   <div class="overflow-auto max-md:w-[98%] w-[85%] bg-red-600 mx-auto py-5 px-7 rounded bg-stone-950 opacity-95">
             <h1 class="max-sm:text-center mb-3 max-sm:text-4xl text-5xl text-red-600 font-semibold">${judul}</h1>
             <h3 class="max-sm:text-center text-slate-200">${penulis}</h3>
-            <div class="mt-5">
-                <img src="${img}" alt="gambar ${judul}" class="float-left mr-7 mb-3 w-[300px] self-center rounded max-sm:mb-5">
+            <div class="mt-5 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
+                <img src="${img}" alt="gambar ${judul}" class="float-left mr-7 mb-3 w-[300px] max-sm:self-center rounded max-sm:mb-5">
                 <p class="inline">${deskripsi}</p>
             </div>
         </div>`;
